b4fadfccbb83b169c95d1cecf8ed679bd124ee67
GuilhermeFinotti2013 <guilhermelfinotti@gmail.com>
2021-07-02 18:34:11 -0300

    Corrigido alertas.

--- src/ProjetoAspNetCore.Data/ORM/CursoDbContext.cs
+++ src/ProjetoAspNetCore.Data/ORM/CursoDbContext.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using ProjetoAspNetCore.Domain.Entities;
-using ProjetoAspNetCore.Domain.Entities;
 using System.Linq;
 
 namespace ProjetoAspNetCore.Data.ORM
--- src/ProjetoAspNetCore.Domain/Interfaces/Repository/IPacienteRepository.cs
+++ src/ProjetoAspNetCore.Domain/Interfaces/Repository/IPacienteRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using ProjetoAspNetCore.Domain.Entities;
-using ProjetoAspNetCore.Domain.Entities;
 using ProjetoAspNetCore.DomainCore.Base;
 
 namespace ProjetoAspNetCore.Domain.Interfaces.Repository
